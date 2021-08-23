59c90d2444453599efcba761cf091dbe917ab7cf
nfcurti <nicolascurti75@gmail.com>
2021-08-23 17:14:30 +0200

    fixes

--- server.js
+++ server.js
@@ -83,5 +83,5 @@ const server = http.createServer((req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${process.env.PORT}/`);
 });
